839290353dce50a94c5d3273836bad4013df0e61
Ezequiel Faustino <ezequielfaustino@gmail.com>
2022-01-31 20:56:05 -0400

    mode script

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-//pegar os botoes e a img e armazenar na const
+//pegar os botoes e a img e armazenar na const;
 const buttons = document.getElementById('buttons');
 const img = document.getElementById('img');
+
+const trafficLight = (event) => {
+    // console.log(event.target.id) //procura o id correspondente e exibe na tela;
+    turnOn[event.target.id]();
+}
+
+const turnOn = { //'ligar' cada semaforo em sua respectiva funcao;
+    'red': () => img.src = './img/vermelho.png',
+    'yellow': () => img.src = './img/amarelo.png',
+    'green': () => img.src = './img/verde.png',
+}
+
+
+//evento de click na const buttons para executar as funcoes de 'ligar' o semaforo
+buttons.addEventListener('click', trafficLight);
